5290a85b9c09f40f8e5f3421bd1535092cf9214a
dai-weiye <167695009+dai-weiye@users.noreply.github.com>
2024-07-27 19:27:01 +0800

    上传项目前后端代码文件

--- utils.py
+++ utils.py
@@ -13,7 +13,3 @@ def get_chat_response(prompt, memory, DASHSCOPE_API_KE):
     response = chain.invoke({"input": prompt})
     return response["response"]
 
-
- #memory = ConversationBufferMemory(return_messages=True)
- #print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("DASHSCOPE_API_KE")))
- #print(get_chat_response("我上一个问题是什么？", memory, os.getenv("DASHSCOPE_API_KE")))
